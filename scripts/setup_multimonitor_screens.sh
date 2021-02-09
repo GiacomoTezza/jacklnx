2c2d77e2f83b754ee15b758c8576dc9238433bb2
GiacomoTezza <tezza.giacomo01@gmail.com>
2021-02-09 17:41:31 +0100

    config and scripts updates

--- scripts/setup_multimonitor_screens.sh
+++ scripts/setup_multimonitor_screens.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
-xrandr --output DP-4 --mode 2560x1440 --rate 144 --primary
+xrandr --output eDP-1 --mode 1920x1080
 
-xrandr --output DP-4 --set TearFree on
+xrandr --output HDMI-1 --mode 2560x1440 --rate 144 --primary --left-of eDP-1
+
+xrandr --output HDMI-1 --set TearFree on
